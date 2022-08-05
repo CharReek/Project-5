6e1cfaeb5a01900d9e654af30726ddf94967cc56
CharReek <charlottereek@outlook.com>
2022-08-05 16:52:53 +0000

    add product view and template

--- products/admin.py
+++ products/admin.py
@@ -12,12 +12,14 @@ class ProductAdmin(admin.ModelAdmin):
         'image',
     )
     ordering = ('name',)
-    
+
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'name',
         'friendly_name',
     )
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
