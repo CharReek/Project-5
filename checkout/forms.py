fb8f55beef16923adfd9c1626da883c94005eae3
CharReek <charlottereek@outlook.com>
2022-08-26 13:55:15 +0000

    added forms.py and customise form

---
+++ checkout/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number', 'country',
+                  'street_address_1', 'street_address_2',
+                  'postcode', 'town_or_city', 'county', )
+
+    def __init__(self, *args, **kwargs):
+        """
+        add place holders and classes removes auto generated
+        labels and sets auto foucs on the first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'postcode': 'Postcode',
+            'town_or_city': 'Town Or City',
+            'street_address_1': 'Street Address 1',
+            'street_address_2': 'Street Address 2',
+            'county': 'County',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
