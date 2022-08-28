2adbfef2dc768871f66edf4d508862eee89aee96
CharReek <charlottereek@outlook.com>
2022-08-28 14:59:30 +0000

    added edit product functionality

--- products/views.py
+++ products/views.py
@@ -86,3 +86,27 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ edit and update products"""
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'This product has been updated')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else: 
+            messages.error(request, 'Failed to update product. Please try again!')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, 'You are currently editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
