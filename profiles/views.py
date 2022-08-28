ea644e06b52f2e3008a7c107622f172fa8beb781
CharReek <charlottereek@outlook.com>
2022-08-28 00:36:39 +0000

    add more detials to profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+
+from checkout.models import Order
 
 
 def profile(request):
@@ -9,12 +12,36 @@ def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has \
+                               sucessfully been updated')
+
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders, 
+        'on_profile_page': True, 
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+    
+    messages.info(request, (
+        f'This order number {order_number} was from a past order.'
+    ))
+    
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
