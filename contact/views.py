7acb0910f4a0e3577dc7c5047bc130f55b9f5e98
CharReek <charlottereek@outlook.com>
2022-09-26 10:34:42 +0000

    added contact form views

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
-from .forms import ContactForm
+from django.shortcuts import render, redirect
+from .forms import ContactForms
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def contact(request):
+    
+    """ a view for users to sumbit forms to admin"""
+    if request.method == "POST":
+        form = ContactForms(request.POST)
+        if form.is_valid():
+            form.save()
+            subject = "Contact Form"
+            body = {
+                'name': form.cleaned_data['name'],
+                'email': form.cleaned_data['email'],
+                'message': form.cleaned_data['message'],
+            }
+            messages = "/n".join(body.values())
+
+            try:
+                send_mail(subject, message, 'admin@example.com', ['admin@example.com'])
+
+            except BadHeaderError:
+                messages.error(request, 'Invalid Header Found')
+                return HttpResponse('Invalid header found')
+            messages.success(request, 'Your form has been successfuly submitted!')
+            return(redirect(contact))
+
+    form = ContactForms
+    return render(request, "contact/contact.html", {'form':form})
